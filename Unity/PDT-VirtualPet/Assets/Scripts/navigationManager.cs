3cd87c4c4c1c6593e1fbe65f52263d89350fbb83
gome70 <gome.z70@hotmail.com>
2020-02-20 23:55:14 +0100

    Added concole navigation rotation to the right.

---
+++ Unity/PDT-VirtualPet/Assets/Scripts/navigationManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class navigationManager : MonoBehaviour
+{
+    private Animator consoleAnimator;
+    private GameObject socket;
+    void Start() 
+    {
+        socket = gameObject;
+        GameObject consoleSocket = GameObject.Find("Concole_Socket");
+        consoleAnimator = consoleSocket.GetComponent<Animator>();
+    }
+    void OnMouseDown()
+    {
+        consoleAnimator.SetTrigger(""+ socket.transform.name);
+    }
+}
